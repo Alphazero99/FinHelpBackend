e044e19b0375355867664e1c51248465acba1bb8
Anshaj Prakash <praansh19@gmail.com>
2024-07-24 18:39:29 +0530

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  origin: 'https://fin-help-frontend.vercel.app', 
   credentials:true,
 };
 
